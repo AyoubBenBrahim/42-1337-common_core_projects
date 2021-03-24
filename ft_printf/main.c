b29d7d1f0d81bf1cb0b2af30b816e40e2f3569cf
Ayoub Bouras <aybouras@student.1337.ma>
2021-03-24 15:04:19 +0100

    repush

---
+++ ft_printf/main.c
@@ -0,0 +1,12 @@
+#include "inc/ft_printf.h"
+#include <stdio.h>
+
+int main()
+{
+	//printf("%llkd\n",LLONG_MAX);
+	// printf("|%-c|\n", 0);
+	// ft_printf("|%-c|\n", 0);
+	// ft_printf(" heello %.2147483647d\n", 45);
+	printf(" heello %.2147483647d\n", 45);
+	//ft_printf("|%.ll..-+d r|\n", LLONG_MAX);
+}
